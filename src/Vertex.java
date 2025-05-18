51b734d648e0131eabcdf6d6212d04c006061c46
Sultan8707 <240254@astanait.edu.kz>
2025-05-18 12:55:59 +0500

    feat: add Vertex class with generic support

---
+++ src/Vertex.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Vertex<T> {
+    private T data;
+
+    public Vertex(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vertex)) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(data, vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
+
